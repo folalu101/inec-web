cc2e2c3e3ccb2fdf69df1f531d3e2b32cd36d87e
A.Adefolalu B <adefolalucode101@gmail.com>
2022-07-13 23:22:12 +0100

    add more js code

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
     $("#voters-reg").submit(function(event){
-        // const fullname1 = $("#fullname-1").Val();
-        // const email1 = $("#email-1").val();
-        // const address1 = $("#address-1").val();
-        // const phone1 = $("#phone-1").val();
+        const fullname1 = $("#fullname-1").Val();
+        const email1 = $("#email-1").val();
+        const address1 = $("#address-1").val();
+        const phone1 = $("#phone-1").val();
         const age1 = $("#age-1").val();
 
-        // $(".fullname-1").text(fullname1);
-        // $(".email-1").text(email1);
-        // $(".address-1").text(address1);
-        // $(".phone-1").text(phone1);
+        $(".fullname-1").text(fullname1);
+        $(".email-1").text(email1);
+        $(".address-1").text(address1);
+        $(".phone-1").text(phone1);
         $(".age-1").text(age1);
 
-        const dob = 18;
+        const age = 18;
 
-        if( dob < 18 ){
+        if( age < 18 ){
             
             window.location.href ="apology.html";
         }
-        else ( dob === 18 || dob > 18 );
+        else 
         {
             window.location.href = "election.html";
         }
 
-        // $("#register-voter").show();
+        $("#register-voter").show();
 
         event.preventDefault();
 
