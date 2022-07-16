1028c95160d3644b550b073722d3dee91bd1329c
A.Adefolalu B <adefolalucode101@gmail.com>
2022-07-16 13:34:43 +0100

    change variable name in js code

--- js/app.js
+++ js/app.js
@@ -1,20 +1,18 @@
 $(document).ready(function() {
     $("#voters-reg").submit(function(event){
-        const fullname1 = $("#fullname-1").Val();
-        const email1 = $("#email-1").val();
-        const address1 = $("#address-1").val();
-        const phone1 = $("#phone-1").val();
+        // const fullname1 = $("#fullname-1").Val();
+        // const email1 = $("#email-1").val();
+        // const address1 = $("#address-1").val();
+        // const phone1 = $("#phone-1").val();
         const age1 = $("#age-1").val();
 
-        $(".fullname-1").text(fullname1);
-        $(".email-1").text(email1);
-        $(".address-1").text(address1);
-        $(".phone-1").text(phone1);
+        // $(".fullname-1").text(fullname1);
+        // $(".email-1").text(email1);
+        // $(".address-1").text(address1);
+        // $(".phone-1").text(phone1);
         $(".age-1").text(age1);
 
-        const age = 18;
-
-        if( age < 18 ){
+        if( age1 < 18 ){
             
             window.location.href ="apology.html";
         }
